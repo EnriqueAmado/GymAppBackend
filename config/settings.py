3a0a5e1cee808908e70f1d19bf40c71f049b479a
KikeAmado <eamadob23@fpcoruna.afundacion.org>
2026-05-15 15:29:50 +0200

    Backend listo: ALLOWED_HOSTS configurado, migraciones hechas y 3 ejercicios creados en BD

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53ov#r1@mtz9!1sb+pyvk^+v_fr2cct3-whvxqgn+#ojeuobgi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
