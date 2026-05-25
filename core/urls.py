3074b3c02e46e14ddf1b7345ccc65f8dd850cd76
KikeAmado <eamadob23@fpcoruna.afundacion.org>
2026-05-25 14:43:10 +0200

    Corregido error de rutas para poder asignar rutinas

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from . import facade_v2
-from .views import ExerciseListView, RoutineListView, WorkoutLogCreateView
+from .views import ExerciseListView, WorkoutLogCreateView
 
 urlpatterns = [
     path('exercises/', ExerciseListView.as_view(), name='exercise-list'),
-    path('routines/', RoutineListView.as_view(), name='routine-list'),
     path('logs/', WorkoutLogCreateView.as_view(), name='log-create'),
     path('register/', facade_v2.api_register_user, name='register_user'),
     path('login/', facade_v2.api_login_user, name='login_user'),
-    path('core/routines/', facade_v2.api_routines, name='api_routines'),
+    path('routines/', facade_v2.api_routines, name='api_routines'),
 ]
